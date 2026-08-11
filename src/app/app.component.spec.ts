b87bb8efd5bfe137e37fb39e41aea3e949c79699
AlaAydi <contact.aydiala@gmail.com>
2026-08-12 00:07:56 +0100

    test: update app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'e-commerce-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('e-commerce-app');
-  });
-
-  it('should render title', () => {
+  it('should render the router outlet container', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, e-commerce-app');
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
