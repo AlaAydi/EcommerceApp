ae444cc09e77d06d85811bb4aa967dfe24112b75
AlaAydi <contact.aydiala@gmail.com>
2026-08-11 23:39:50 +0100

    feat: update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,12 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { CartDrawerComponent } from './shared/components/cart-drawer/cart-drawer.component';
-import { WishlistDrawerComponent } from './shared/components/wishlist-drawer/wishlist-drawer.component';
-import { AuthModalComponent } from './shared/components/auth-modal/auth-modal.component';
-import { RegisterSuccessModalComponent } from './shared/components/register-success-modal/register-success-modal.component';
-import { QuickViewModalComponent } from './shared/components/quick-view-modal/quick-view-modal.component';
-import { ToastNotificationsComponent } from './shared/components/toast-notifications/toast-notifications.component';
-import { HomeComponent } from './features/home/home.component';
-import { ProductDetailComponent } from './features/product-detail/product-detail.component';
-import { CheckoutComponent } from './features/checkout/checkout.component';
-import { AdminDashboardComponent } from './features/admin/admin-dashboard.component';
-import { ProductService } from './core/services/product.service';
-import { AdminService } from './core/services/admin.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,
-    HeaderComponent,
-    FooterComponent,
-    CartDrawerComponent,
-    WishlistDrawerComponent,
-    AuthModalComponent,
-    RegisterSuccessModalComponent,
-    QuickViewModalComponent,
-    ToastNotificationsComponent,
-    HomeComponent,
-    ProductDetailComponent,
-    CheckoutComponent,
-    AdminDashboardComponent
+    RouterOutlet
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: '<router-outlet></router-outlet>'
 })
-export class AppComponent implements OnInit {
-  private productService = inject(ProductService);
-  private adminService = inject(AdminService);
-
-  selectedProduct = this.productService.selectedProduct;
-  isAdminView = this.adminService.isAdminView;
-  isCheckoutOpen = false;
-
-  ngOnInit() {
-    window.addEventListener('openCheckout', () => {
-      this.isCheckoutOpen = true;
-    });
-  }
-}
+export class AppComponent {}
