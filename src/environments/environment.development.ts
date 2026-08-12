6cb450391dd54a0439910c416cff1d2a09b655f3
AlaAydi <contact.aydiala@gmail.com>
2026-08-12 01:32:20 +0100

    feat: secure vercel env config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  },
+  emailjs: {
+    serviceId: '',
+    templateId: '',
+    publicKey: ''
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  firebase: {
+  "apiKey": "",
+  "authDomain": "",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "",
+  "measurementId": ""
+},
+  emailjs: {
+  "serviceId": "",
+  "templateId": "",
+  "publicKey": ""
+}
+};
